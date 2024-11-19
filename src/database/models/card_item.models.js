64989301eca41cd40ba1fcfbc555fe2e52d851ca
Ismoil <muhiddinovismoil2@gmail.com>
2024-11-20 00:20:43 +0500

    few changes

--- src/database/models/card_item.models.js
+++ src/database/models/card_item.models.js
@@ -1,23 +1,24 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from 'mongoose'
 
-const cardItem = new Schema({
-    cart_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Cart",
-        required:true
+const cardItem = new Schema(
+    {
+        cart_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Cart',
+            required: true,
+        },
+        product_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+            required: true,
+        },
+        quantity: {
+            type: Number,
+        },
     },
-    product_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Product",
-        required:true
+    {
+        required: true,
     },
-    quantity:{
-        type:Number
-    }
-},
-{
-    required:true
-})
+)
 
-
-export default mongoose.model('CardItem', cardItem)
+export const CardItem = mongoose.model('CardItem', cardItem)
--- src/database/models/product.models.js
+++ src/database/models/product.models.js
@@ -1,37 +1,39 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const productSchema = new mongoose.Schema({
-    product_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Categories",
-        required:true
+const productSchema = new mongoose.Schema(
+    {
+        product_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Categories',
+            required: true,
+        },
+        title: {
+            type: String,
+        },
+        picture: {
+            type: String,
+        },
+        summary: {
+            type: String,
+        },
+        description: {
+            type: String,
+        },
+        price: {
+            type: Number,
+        },
+        discount_type: {
+            type: String,
+            enum: ['aksiya qulpiniso', 'chegirma'],
+        },
+        discount_value: {
+            type: Number,
+        },
+        tags: {
+            type: Array,
+        },
     },
-    title:{
-    type:String
-   },
-   picture:{
-    type:String
-   },
-   summary:{
-    type:String
-   },
-   description:{
-    type:String
-   },
-   price:{
-    type:Number
-   },
-   discount_type:{
-    type:String,
-    enum:["aksiya qulpiniso", "chegirma"]
-   },
-   discount_value:{
-    type:Number
-   },
-   tags:{
-    type:Array
-   }
-},
-{ timestamps: true },)
+    { timestamps: true },
+)
 
-export default mongoose.model('Product', productSchema)
+export const Product = mongoose.model('Product', productSchema)
